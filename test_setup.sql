f277719f10f19ec9ba9f4ec5789a1bf00b2a9709
Jack Christensen <jack@jackchristensen.com>
2013-07-30 07:30:50 -0500

    Remove unused test_setup.sql

--- test_setup.sql
+++
@@ -1,9 +0,0 @@
-drop database if exists pgx_test;
-drop user if exists pgx_none;
-drop user if exists pgx_pw;
-drop user if exists pgx_md5;
-
-create user pgx_none;
-create user pgx_pw password 'secret';
-create user pgx_md5 password 'secret';
-create database pgx_test;
