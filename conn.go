f6b1f4dd99a44037dd15a80e8728e66a097f81a2
Carlos Vargas <cvargas@banwire.com>
2015-07-14 16:35:23 -0500

    JsonOId added in sendSimpleQuery for encoder

--- conn.go
+++ conn.go
@@ -748,6 +748,8 @@ func (c *Conn) sendPreparedQuery(ps *PreparedStatement, arguments ...interface{}
 				err = encodeTimestampArray(wbuf, arguments[i], TimestampTzOid)
 			case OidOid:
 				err = encodeOid(wbuf, arguments[i])
+			case JsonOid:
+				err = encodeText(wbuf, arguments[i])
 			default:
 				return SerializationError(fmt.Sprintf("Cannot encode %T into oid %v - %T must implement Encoder or be converted to a string", arg, oid, arg))
 			}
