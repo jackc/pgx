f49b92d5a89033429c07a3fcfe74fd76426c2a13
Manni Wood <manni.lee.wood@gmail.com>
2016-09-10 19:47:51 -0400

    Fixes typo

--- values.go
+++ values.go
@@ -342,7 +342,7 @@ func (n NullInt32) Encode(w *WriteBuf, oid Oid) error {
 //
 // it is the data type of the xmin and xmax hidden system columns.
 //
-// It is currently implemented as an unsigned for byte integer.
+// It is currently implemented as an unsigned four byte integer.
 // Its definition can be found in src/include/postgres_ext.h as TransactionId
 // in the PostgreSQL sources.
 type Xid uint32
@@ -394,7 +394,7 @@ func (n NullXid) Encode(w *WriteBuf, oid Oid) error {
 //
 // it is the data type of the cmin and cmax hidden system columns.
 //
-// It is currently implemented as an unsigned for byte integer.
+// It is currently implemented as an unsigned four byte integer.
 // Its definition can be found in src/include/c.h as CommandId
 // in the PostgreSQL sources.
 type Cid uint32
