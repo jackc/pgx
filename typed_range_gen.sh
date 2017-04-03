066562fc89899eac7e67acc5194f285826e9a734
Jack Christensen <jack@jackchristensen.com>
2017-04-03 07:35:19 -0500

    Add pgtype.Numrange

--- typed_range_gen.sh
+++ typed_range_gen.sh
@@ -3,4 +3,5 @@ erb range_type=Int8range element_type=Int8 typed_range.go.erb > int8range.go
 erb range_type=Tsrange element_type=Timestamp typed_range.go.erb > tsrange.go
 erb range_type=Tstzrange element_type=Timestamptz typed_range.go.erb > tstzrange.go
 erb range_type=Daterange element_type=Date typed_range.go.erb > daterange.go
+erb range_type=Numrange element_type=Numeric typed_range.go.erb > numrange.go
 goimports -w *range.go
