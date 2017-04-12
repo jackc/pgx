e76cf5617fa7bbdb4c502ee2e96c08665711f5de
Jack Christensen <jack@jackchristensen.com>
2017-04-11 20:16:41 -0500

    Skip line tests on when server version < PG 9.4

--- line_test.go
+++ line_test.go
@@ -3,10 +3,24 @@ package pgtype_test
 import (
 	"testing"
 
+	version "github.com/hashicorp/go-version"
 	"github.com/jackc/pgx/pgtype"
 )
 
 func TestLineTranscode(t *testing.T) {
+	conn := mustConnectPgx(t)
+	serverVersion, err := version.NewVersion(conn.RuntimeParams["server_version"])
+	if err != nil {
+		t.Fatalf("cannot get server version: %v", err)
+	}
+	mustClose(t, conn)
+
+	minVersion := version.Must(version.NewVersion("9.4"))
+
+	if serverVersion.LessThan(minVersion) {
+		t.Skipf("Skipping line test for server version %v", serverVersion)
+	}
+
 	testSuccessfulTranscode(t, "line", []interface{}{
 		&pgtype.Line{
 			A: 1.23, B: 4.56, C: 7.89,
