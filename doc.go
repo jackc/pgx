2d209bd579c721e74cdc7614b1e643958fbae0e3
Jack Christensen <jack@jackchristensen.com>
2017-05-06 08:53:37 -0500

    Remove read functions from pgio and update docs

--- doc.go
+++ doc.go
@@ -1,8 +1,6 @@
-// Package pgio a extremely low-level IO toolkit for the PostgreSQL wire protocol.
+// Package pgio is a low-level toolkit building messages in the PostgreSQL wire protocol.
 /*
-pgio provides functions for reading and writing integers from io.Reader and
-io.Writer while doing byte order conversion. It publishes interfaces which
-readers and writers may implement to decode and encode messages with the minimum
-of memory allocations.
+pgio provides functions for appending integers to a []byte while doing byte
+order conversion.
 */
 package pgio
