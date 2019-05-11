c418d45f756b39b8771f0f002c1e4b1703591211
Jack Christensen <jack@jackchristensen.com>
2019-05-11 11:50:50 -0500

    Remove unused code

--- large_objects.go
+++ large_objects.go
@@ -16,22 +16,6 @@ type LargeObjects struct {
 	tx *Tx
 }
 
-const largeObjectFns = `select proname, oid from pg_catalog.pg_proc
-where proname in (
-'lo_open',
-'lo_close',
-'lo_create',
-'lo_unlink',
-'lo_lseek',
-'lo_lseek64',
-'lo_tell',
-'lo_tell64',
-'lo_truncate',
-'lo_truncate64',
-'loread',
-'lowrite')
-and pronamespace = (select oid from pg_catalog.pg_namespace where nspname = 'pg_catalog')`
-
 // LargeObjects returns a LargeObjects instance for the transaction.
 func (tx *Tx) LargeObjects() LargeObjects {
 	return LargeObjects{tx: tx}
