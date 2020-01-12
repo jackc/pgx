05d81d0b9fd4fe54a28386039dda13c5516268f5
Jack Christensen <jack@jackchristensen.com>
2020-01-11 18:27:03 -0600

    Add PgError.SQLState method

--- messages.go
+++ messages.go
@@ -105,6 +105,11 @@ func (pe PgError) Error() string {
 	return pe.Severity + ": " + pe.Message + " (SQLSTATE " + pe.Code + ")"
 }
 
+// SQLState returns the SQLState of the error.
+func (pe PgError) SQLState() string {
+	return pe.Code
+}
+
 // Notice represents a notice response message reported by the PostgreSQL
 // server. Be aware that this is distinct from LISTEN/NOTIFY notification.
 type Notice PgError
