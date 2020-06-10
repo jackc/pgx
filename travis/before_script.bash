de77c70f48df17454d767fb058e9e5a2ab9c89a6
Jacob Powers <jpowers@23andme.com>
2020-06-10 09:01:34 -0700

    enable hstore extension before running tests

--- travis/before_script.bash
+++ travis/before_script.bash
@@ -7,4 +7,5 @@ then
   psql -U postgres -c 'create database pgx_test'
   psql -U postgres pgx_test -c 'create domain uint64 as numeric(20,0)'
   psql -U postgres -c "create user pgx_md5 SUPERUSER PASSWORD 'secret'"
+  psql -U postgres pgx_test -c 'create extension if not exists hstore;'
 fi
