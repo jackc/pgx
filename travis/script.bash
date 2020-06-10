6d62aec6b1e288ed2a646ddb004f645ea02eb4ac
Jacob Powers <jpowers@23andme.com>
2020-06-09 18:31:49 -0700

    remove irrelevant test from pgx

--- travis/script.bash
+++ travis/script.bash
@@ -2,10 +2,4 @@
 # source: https://github.com/jackc/pgx/blob/master/travis/script.bash
 set -eux
 
-if [ "${PGVERSION-}" != "" ]
-then
-  go test -v -race ./...
-elif [ "${CRATEVERSION-}" != "" ]
-then
-  go test -v -race -run 'TestCrateDBConnect'
-fi
+go test -v -race ./...
