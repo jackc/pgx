0577a157b1174cb51c3b958904928c0d6f2c1844
Oleg Kovalov <iamolegkovalov@gmail.com>
2020-10-05 20:22:45 +0200

    remove unused definitions on messages.go

--- messages.go
+++ messages.go
@@ -3,14 +3,7 @@ package pgx
 import (
 	"database/sql/driver"
 
-	"github.com/jackc/pgio"
 	"github.com/jackc/pgtype"
-)
-
-const (
-	copyData = 'd'
-	copyFail = 'f'
-	copyDone = 'c'
 )
 
 func convertDriverValuers(args []interface{}) ([]interface{}, error) {
@@ -27,16 +20,4 @@ func convertDriverValuers(args []interface{}) ([]interface{}, error) {
 		}
 	}
 	return args, nil
-}
-
-// appendQuery appends a PostgreSQL wire protocol query message to buf and returns it.
-func appendQuery(buf []byte, query string) []byte {
-	buf = append(buf, 'Q')
-	sp := len(buf)
-	buf = pgio.AppendInt32(buf, -1)
-	buf = append(buf, query...)
-	buf = append(buf, 0)
-	pgio.SetInt32(buf[sp:], int32(len(buf[sp:])))
-
-	return buf
 }
