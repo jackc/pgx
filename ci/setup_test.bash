eb322859067bf699fbfe8e8a5a8c6c89a1f5ff7e
Jack Christensen <jack@jackchristensen.com>
2021-01-30 16:22:38 -0600

    Use native PostgreSQL package
    Also remove travis integration.

--- ci/setup_test.bash
+++ ci/setup_test.bash
@@ -24,6 +24,18 @@ then
     echo "max_replication_slots=5" >> /etc/postgresql/$PGVERSION/main/postgresql.conf
   fi
   sudo /etc/init.d/postgresql restart
+
+  # The tricky test user, below, has to actually exist so that it can be used in a test
+  # of aclitem formatting. It turns out aclitems cannot contain non-existing users/roles.
+  psql -U postgres -c 'create database pgx_test'
+  psql -U postgres pgx_test -c 'create extension hstore'
+  psql -U postgres pgx_test -c 'create domain uint64 as numeric(20,0)'
+  psql -U postgres -c "create user pgx_ssl SUPERUSER PASSWORD 'secret'"
+  psql -U postgres -c "create user pgx_md5 SUPERUSER PASSWORD 'secret'"
+  psql -U postgres -c "create user pgx_pw  SUPERUSER PASSWORD 'secret'"
+  psql -U postgres -c "create user travis"
+  psql -U postgres -c "create user pgx_replication with replication password 'secret'"
+  psql -U postgres -c "create user \" tricky, ' } \"\" \\ test user \" superuser password 'secret'"
 fi
 
 if [ "${CRATEVERSION-}" != "" ]
