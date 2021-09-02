55195b3a64647fbbe017a9706d9bd7585959c13f
Jack Christensen <jack@jackchristensen.com>
2021-09-02 15:55:50 -0500

    Add Numeric.Getter

--- numeric.go
+++ numeric.go
@@ -62,11 +62,13 @@ type Numeric struct {
 	Valid            bool
 
 	NumericDecoderWrapper func(interface{}) NumericDecoder
+	Getter                func(Numeric) interface{}
 }
 
 func (n *Numeric) NewTypeValue() Value {
 	return &Numeric{
 		NumericDecoderWrapper: n.NumericDecoderWrapper,
+		Getter:                n.Getter,
 	}
 }
 
@@ -258,6 +260,10 @@ func (dst *Numeric) Set(src interface{}) error {
 }
 
 func (dst Numeric) Get() interface{} {
+	if dst.Getter != nil {
+		return dst.Getter(dst)
+	}
+
 	if !dst.Valid {
 		return nil
 	}
