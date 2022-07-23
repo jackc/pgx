83780b85b5e09fed5fadd3c94f30e9ec2bb5c574
Jack Christensen <jack@jackchristensen.com>
2022-07-23 08:54:59 -0500

    Remove pgx logging code moved to tracelog

--- log/testingadapter/adapter.go
+++ log/testingadapter/adapter.go
@@ -6,7 +6,7 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/tracelog"
 )
 
 // TestingLogger interface defines the subset of testing.TB methods used by this
@@ -23,7 +23,7 @@ func NewLogger(l TestingLogger) *Logger {
 	return &Logger{l: l}
 }
 
-func (l *Logger) Log(ctx context.Context, level pgx.LogLevel, msg string, data map[string]any) {
+func (l *Logger) Log(ctx context.Context, level tracelog.LogLevel, msg string, data map[string]any) {
 	logArgs := make([]any, 0, 2+len(data))
 	logArgs = append(logArgs, level, msg)
 	for k, v := range data {
