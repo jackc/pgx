639fb28846781465bc52b373c3f73dda3873932c
Peter Feichtinger <shippo@gmx.at>
2022-09-20 12:42:42 +0200

    Fix typo

--- named_args.go
+++ named_args.go
@@ -12,8 +12,8 @@ import (
 //
 // For example, the following two queries are equivalent:
 //
-// 	conn.Query(ctx, "select * from widgets where foo = @foo and bar = @bar", pgx.NamedArgs{"foo": 1, "bar": 2}))
-// 	conn.Query(ctx, "select * from widgets where foo = $1 and bar = $2", 1, 2}))
+// 	conn.Query(ctx, "select * from widgets where foo = @foo and bar = @bar", pgx.NamedArgs{"foo": 1, "bar": 2})
+// 	conn.Query(ctx, "select * from widgets where foo = $1 and bar = $2", 1, 2)
 type NamedArgs map[string]any
 
 // RewriteQuery implements the QueryRewriter interface.
