9eaeb51e305fa7bed7c74a290242f9d020a55129
Jack Christensen <jack@jackchristensen.com>
2022-10-29 17:55:13 -0500

    Fix CI PostgreSQL user permissions

--- testsetup/postgresql_setup.sql
+++ testsetup/postgresql_setup.sql
@@ -3,14 +3,14 @@ create extension hstore;
 create domain uint64 as numeric(20,0);
 
 -- Create users for different types of connections and authentication.
-create user pgx_ssl PASSWORD 'secret';
+create user pgx_ssl PASSWORD 'secret' with superuser;
 set password_encryption = md5;
-create user pgx_md5 PASSWORD 'secret';
+create user pgx_md5 PASSWORD 'secret' with superuser;
 set password_encryption = 'scram-sha-256';
-create user pgx_pw PASSWORD 'secret';
-create user pgx_scram PASSWORD 'secret';
+create user pgx_pw PASSWORD 'secret' with superuser;
+create user pgx_scram PASSWORD 'secret' with superuser;
 \set whoami `whoami`
-create user :whoami; -- unix domain socket user
+create user :whoami with superuser; -- unix domain socket user
 
 
 -- The tricky test user, below, has to actually exist so that it can be used in a test
