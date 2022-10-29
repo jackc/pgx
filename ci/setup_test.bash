97d1012f4226ff180661332f0812a5e4b1d56a1d
Jack Christensen <jack@jackchristensen.com>
2022-10-29 17:27:39 -0500

    Use testsetup/postgresql_setup.sql in CI

--- ci/setup_test.bash
+++ ci/setup_test.bash
@@ -18,18 +18,8 @@ then
   fi
   sudo /etc/init.d/postgresql restart
 
-  psql -U postgres -c 'create database pgx_test'
-  psql -U postgres pgx_test -c 'create extension hstore'
-  psql -U postgres pgx_test -c 'create domain uint64 as numeric(20,0)'
-  psql -U postgres -c "create user pgx_ssl SUPERUSER PASSWORD 'secret'"
-  psql -U postgres -c "create user pgx_md5 SUPERUSER PASSWORD 'secret'"
-  psql -U postgres -c "create user pgx_pw  SUPERUSER PASSWORD 'secret'"
-  psql -U postgres -c "create user `whoami`"
-  psql -U postgres -c "create user pgx_replication with replication password 'secret'"
-
-  # The tricky test user, below, has to actually exist so that it can be used in a test
-  # of aclitem formatting. It turns out aclitems cannot contain non-existing users/roles.
-  psql -U postgres -c "create user \" tricky, ' } \"\" \\ test user \" superuser password 'secret'"
+  createdb -U postgres pgx_test
+  psql -U postgres -f testsetup/postgresql_setup.sql pgx_test
 fi
 
 if [[ "${PGVERSION-}" =~ ^cockroach ]]
