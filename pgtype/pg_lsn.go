5ffe8df4adf9e82cb02fe53c874f767e53090f48
Hiroaki Nakamura <hnakamur@gmail.com>
2022-12-09 21:49:46 +0900

    Rewrite PgLSNCodec and remove ScanText and TextValue of PgLSN

--- pgtype/pg_lsn.go
+++ pgtype/pg_lsn.go
@@ -11,6 +11,25 @@ type PgLSN struct {
 	Valid bool
 }
 
+func parsePgLSN(s string) (uint64, error) {
+	var hi, lo uint32
+	n, err := fmt.Sscanf(s, "%X/%X", &hi, &lo)
+	if err != nil {
+		return 0, err
+	}
+	if n != 2 {
+		return 0, errors.New("invalid pg_lsn value")
+	}
+	return uint64(hi)<<32 | uint64(lo), nil
+}
+
+func (src *PgLSN) String() string {
+	if !src.Valid {
+		return ""
+	}
+	return fmt.Sprintf("%X/%X", src.LSN>>32, uint32(src.LSN))
+}
+
 func (n *PgLSN) ScanUint64(v Uint64) error {
 	*n = PgLSN{
 		LSN:   v.Uint64,
@@ -26,110 +45,162 @@ func (n PgLSN) Uint64Value() (Uint64, error) {
 	}, nil
 }
 
-func (n *PgLSN) ScanText(v Text) error {
-	if v.Valid {
-		i, err := parsePgLSN(v.String)
-		if err != nil {
-			return err
+type PgLSNCodec struct{}
+
+func (PgLSNCodec) FormatSupported(format int16) bool {
+	return format == TextFormatCode || format == BinaryFormatCode
+}
+
+func (PgLSNCodec) PreferredFormat() int16 {
+	return TextFormatCode
+}
+
+func (c PgLSNCodec) PlanEncode(m *Map, oid uint32, format int16, value any) EncodePlan {
+	switch format {
+	case BinaryFormatCode:
+		return Uint64Codec{}.PlanEncode(m, oid, format, value)
+	case TextFormatCode:
+		switch value.(type) {
+		case PgLSN:
+			return encodePlanPgLSNCodecEitherFormatPgLSN{}
+		case string:
+			return encodePlanPgLSNCodecEitherFormatString{}
+		case []byte:
+			return encodePlanPgLSNCodecEitherFormatByteSlice{}
 		}
-		*n = PgLSN{
-			LSN:   i,
-			Valid: true,
+
+		// Because anything can be marshalled the normal wrapping in Map.PlanScan doesn't get a chance to run. So try the
+		// appropriate wrappers here.
+		for _, f := range []TryWrapEncodePlanFunc{
+			TryWrapDerefPointerEncodePlan,
+			TryWrapFindUnderlyingTypeEncodePlan,
+		} {
+			if wrapperPlan, nextValue, ok := f(value); ok {
+				if nextPlan := c.PlanEncode(m, oid, format, nextValue); nextPlan != nil {
+					wrapperPlan.SetNext(nextPlan)
+					return wrapperPlan
+				}
+			}
 		}
-		return nil
 	}
 
-	*n = PgLSN{
-		LSN:   0,
-		Valid: false,
-	}
 	return nil
 }
 
-func parsePgLSN(s string) (uint64, error) {
-	var hi, lo uint32
-	n, err := fmt.Sscanf(s, "%X/%X", &hi, &lo)
-	if err != nil {
-		return 0, err
+type encodePlanPgLSNCodecEitherFormatPgLSN struct{}
+
+func (encodePlanPgLSNCodecEitherFormatPgLSN) Encode(value any, buf []byte) (newBuf []byte, err error) {
+	v := value.(PgLSN)
+	if !v.Valid {
+		return nil, nil
 	}
-	if n != 2 {
-		return 0, errors.New("invalid pg_lsn value")
-	}
-	return uint64(hi)<<32 | uint64(lo), nil
+
+	pgLSNString := v.String()
+	buf = append(buf, pgLSNString...)
+	return buf, nil
 }
 
-func (n PgLSN) TextValue() (Text, error) {
-	return Text{
-		String: n.String(),
-		Valid:  n.Valid,
-	}, nil
+type encodePlanPgLSNCodecEitherFormatString struct{}
+
+func (encodePlanPgLSNCodecEitherFormatString) Encode(value any, buf []byte) (newBuf []byte, err error) {
+	pgLSNString := value.(string)
+	buf = append(buf, pgLSNString...)
+	return buf, nil
 }
 
-func (src *PgLSN) String() string {
-	if !src.Valid {
-		return ""
+type encodePlanPgLSNCodecEitherFormatByteSlice struct{}
+
+func (encodePlanPgLSNCodecEitherFormatByteSlice) Encode(value any, buf []byte) (newBuf []byte, err error) {
+	pgLSNBytes := value.([]byte)
+	if pgLSNBytes == nil {
+		return nil, nil
 	}
-	return fmt.Sprintf("%X/%X", src.LSN>>32, uint32(src.LSN))
+
+	buf = append(buf, pgLSNBytes...)
+	return buf, nil
 }
 
-// Scan implements the database/sql Scanner interface.
-func (dst *PgLSN) Scan(src any) error {
+func (PgLSNCodec) PlanScan(m *Map, oid uint32, format int16, target any) ScanPlan {
+	switch format {
+	case BinaryFormatCode:
+		return Uint64Codec{}.PlanScan(m, oid, format, target)
+	case TextFormatCode:
+		switch target.(type) {
+		case *PgLSN:
+			return scanPlanPgLSNBytesToPgLSN{}
+		case *string:
+			return scanPlanAnyToString{}
+		case *[]byte:
+			return scanPlanPgLSNToByteSlice{}
+		}
+	}
+
+	return nil
+}
+
+type scanPlanPgLSNBytesToPgLSN struct{}
+
+func (scanPlanPgLSNBytesToPgLSN) Scan(src []byte, dst any) error {
+	dstPgLSN := dst.(*PgLSN)
 	if src == nil {
-		*dst = PgLSN{}
+		*dstPgLSN = PgLSN{LSN: 0, Valid: false}
 		return nil
+	}
+
+	n, err := parsePgLSN(string(src))
+	if err != nil {
+		return err
+	}
+	*dstPgLSN = PgLSN{LSN: n, Valid: true}
+	return nil
+}
+
+type scanPlanPgLSNToByteSlice struct{}
+
+func (scanPlanPgLSNToByteSlice) Scan(src []byte, dst any) error {
+	dstBuf := dst.(*[]byte)
+	if src == nil {
+		*dstBuf = nil
+		return nil
+	}
+
+	*dstBuf = make([]byte, len(src))
+	copy(*dstBuf, src)
+	return nil
+}
+
+func (PgLSNCodec) DecodeDatabaseSQLValue(m *Map, oid uint32, format int16, src []byte) (driver.Value, error) {
+	if src == nil {
+		return nil, nil
+	}
+
+	dstBuf := make([]byte, len(src))
+	copy(dstBuf, src)
+	return dstBuf, nil
+}
+
+func (PgLSNCodec) DecodeValue(tm *Map, oid uint32, format int16, src []byte) (any, error) {
+	if src == nil {
+		return nil, nil
 	}
 
 	var n uint64
-
-	switch src := src.(type) {
-	case int64:
-		n = uint64(src)
-	case uint64:
-		n = src
-	case string:
-		var err error
-		n, err = parsePgLSN(src)
+	switch format {
+	case BinaryFormatCode:
+		an, err := Uint64Codec{}.DecodeValue(tm, oid, format, src)
 		if err != nil {
-			return err
+			return nil, err
+		}
+		n = an.(uint64)
+	case TextFormatCode:
+		var err error
+		n, err = parsePgLSN(string(src))
+		if err != nil {
+			return nil, err
 		}
 	default:
-		return fmt.Errorf("cannot scan %T", src)
+		return nil, fmt.Errorf("unknown format code: %v", format)
 	}
 
-	*dst = PgLSN{LSN: n, Valid: true}
-
-	return nil
-}
-
-// Value implements the database/sql/driver Valuer interface.
-func (src PgLSN) Value() (driver.Value, error) {
-	if !src.Valid {
-		return nil, nil
-	}
-	// Return int64 instead of uint64 because uint64 is not allowed at
-	// https://pkg.go.dev/database/sql/driver#Value
-	return int64(src.LSN), nil
-}
-
-type PgLSNCodec struct {
-	Uint64Codec
-}
-
-func (PgLSNCodec) DecodeValue(tm *Map, oid uint32, format int16, src []byte) (interface{}, error) {
-	if src == nil {
-		return nil, nil
-	}
-
-	var target PgLSN
-	scanPlan := tm.PlanScan(oid, format, &target)
-	if scanPlan == nil {
-		return nil, fmt.Errorf("PlanScan did not find a plan")
-	}
-
-	err := scanPlan.Scan(src, &target)
-	if err != nil {
-		return nil, err
-	}
-
-	return target, nil
+	return PgLSN{LSN: n, Valid: true}, nil
 }
--- pgtype/pg_lsn_test.go
+++ pgtype/pg_lsn_test.go
@@ -11,22 +11,21 @@ import (
 func TestPgLSNCodec(t *testing.T) {
 	skipCockroachDB(t, "Server does not support pg_lsn type")
 
-	pgxtest.RunValueRoundTripTests(context.Background(), t, defaultConnTestRunner, pgxtest.KnownOIDQueryExecModes, "pg_lsn", []pgxtest.ValueRoundTripTest{
-		{
-			pgtype.PgLSN{LSN: pgtype.TextOID, Valid: true},
-			new(pgtype.PgLSN),
-			isExpectedEq(pgtype.PgLSN{LSN: pgtype.TextOID, Valid: true}),
-		},
-		{pgtype.PgLSN{}, new(pgtype.PgLSN), isExpectedEq(pgtype.PgLSN{})},
-		{nil, new(pgtype.PgLSN), isExpectedEq(pgtype.PgLSN{})},
+	pgxtest.RunValueRoundTripTests(context.Background(), t, defaultConnTestRunner, nil, "pg_lsn", []pgxtest.ValueRoundTripTest{
+		{nil, new(pgtype.PgLSN), isExpectedEq(pgtype.PgLSN{LSN: 0, Valid: false})},
+		{[]byte(nil), new(pgtype.PgLSN), isExpectedEq(pgtype.PgLSN{LSN: 0, Valid: false})},
+		{nil, new([]byte), isExpectedEqBytes([]byte(nil))},
+		{[]byte(nil), new([]byte), isExpectedEqBytes([]byte(nil))},
+		{nil, new(*string), isExpectedEq((*string)(nil))},
+		{[]byte(nil), new(*string), isExpectedEq((*string)(nil))},
 	})
-	pgxtest.RunValueRoundTripTests(context.Background(), t, defaultConnTestRunner, pgxtest.KnownOIDQueryExecModes, "text", []pgxtest.ValueRoundTripTest{
-		{
-			pgtype.PgLSN{LSN: pgtype.TextOID, Valid: true},
-			new(pgtype.PgLSN),
-			isExpectedEq(pgtype.PgLSN{LSN: pgtype.TextOID, Valid: true}),
-		},
-		{pgtype.PgLSN{}, new(pgtype.PgLSN), isExpectedEq(pgtype.PgLSN{})},
-		{nil, new(pgtype.PgLSN), isExpectedEq(pgtype.PgLSN{})},
+
+	pgxtest.RunValueRoundTripTests(context.Background(), t, defaultConnTestRunner, pgxtest.KnownOIDQueryExecModes, "pg_lsn", []pgxtest.ValueRoundTripTest{
+		{[]byte("1F/FFFFFFFF"), new(pgtype.PgLSN), isExpectedEq(pgtype.PgLSN{LSN: 0x1F_FFFFFFFF, Valid: true})},
+		{[]byte("1F/FFFFFFFF"), new([]byte), isExpectedEqBytes([]byte("1F/FFFFFFFF"))},
+		{[]byte("1F/FFFFFFFF"), new(string), isExpectedEq("1F/FFFFFFFF")},
+		{[]byte("FFFFFFFF/FFFFFFFF"), new(pgtype.PgLSN), isExpectedEq(pgtype.PgLSN{LSN: 0xFFFFFFFF_FFFFFFFF, Valid: true})},
+		{[]byte("FFFFFFFF/FFFFFFFF"), new([]byte), isExpectedEqBytes([]byte("FFFFFFFF/FFFFFFFF"))},
+		{[]byte("FFFFFFFF/FFFFFFFF"), new(string), isExpectedEq("FFFFFFFF/FFFFFFFF")},
 	})
 }
