cf78472ce5bfe0069349978db1f1f4a03e8ca953
Jack Christensen <jack@jackchristensen.com>
2023-01-31 20:10:05 -0600

    Use unix build tag
    With Go 1.19 available we can use a simpler build tag.

--- internal/nbconn/nbconn_fake_non_block.go
+++ internal/nbconn/nbconn_fake_non_block.go
@@ -1,8 +1,6 @@
-//go:build !(aix || android || darwin || dragonfly || freebsd || hurd || illumos || ios || linux || netbsd || openbsd || solaris)
+//go:build !unix
 
 package nbconn
-
-// Not using unix build tag for support on Go 1.18.
 
 func (c *NetConn) realNonblockingWrite(b []byte) (n int, err error) {
 	return c.fakeNonblockingWrite(b)
