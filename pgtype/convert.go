e445f8e9a4a705894d8fbb2d89d65d8ec2012329
Sergej Brazdeikis <sergej.brazdeikis@gmail.com>
2023-03-11 16:15:52 +0100

    Fix typo in error message `uint32` -> `uint16`

--- pgtype/convert.go
+++ pgtype/convert.go
@@ -262,7 +262,7 @@ func int64AssignTo(srcVal int64, srcValid bool, dst any) error {
 			*v = uint8(srcVal)
 		case *uint16:
 			if srcVal < 0 {
-				return fmt.Errorf("%d is less than zero for uint32", srcVal)
+				return fmt.Errorf("%d is less than zero for uint16", srcVal)
 			} else if srcVal > math.MaxUint16 {
 				return fmt.Errorf("%d is greater than maximum value for uint16", srcVal)
 			}
