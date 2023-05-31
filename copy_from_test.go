b4314ddaf7956657e979c26d84bd432384cc8d7e
Nicola Murino <nicola.murino@gmail.com>
2023-05-31 14:40:19 +0200

    TestConnCopyFromSlowFailRace: increase context timeout
    On Windows time.Sleep(time.Millisecond) will sleep for 15 milliseconds

--- copy_from_test.go
+++ copy_from_test.go
@@ -640,7 +640,7 @@ func (fs *slowFailRaceSource) Err() error {
 func TestConnCopyFromSlowFailRace(t *testing.T) {
 	t.Parallel()
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
 	conn := mustConnectString(t, os.Getenv("PGX_TEST_DATABASE"))
