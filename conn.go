a61517a83b81a472d4ace9c48d5ff5af5b608048
Jack Christensen <jack@jackchristensen.com>
2023-09-28 20:00:02 -0500

    SendBatch should pass ctx to StartPipeline
    https://github.com/jackc/pgx/issues/1748

--- conn.go
+++ conn.go
@@ -1090,7 +1090,7 @@ func (c *Conn) sendBatchQueryExecModeDescribeExec(ctx context.Context, b *Batch)
 }
 
 func (c *Conn) sendBatchExtendedWithDescription(ctx context.Context, b *Batch, distinctNewQueries []*pgconn.StatementDescription, sdCache stmtcache.Cache) (pbr *pipelineBatchResults) {
-	pipeline := c.pgConn.StartPipeline(context.Background())
+	pipeline := c.pgConn.StartPipeline(ctx)
 	defer func() {
 		if pbr != nil && pbr.err != nil {
 			pipeline.Close()
