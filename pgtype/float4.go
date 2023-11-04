d3fb6e00da0c7bc7fce3b22a895a0fb72e39322e
Kirill Mironov <22131753+KirillMironov@users.noreply.github.com>
2023-11-04 14:00:41 +0300

    implement json.Marshaler and json.Unmarshaler for Float4, Float8

--- pgtype/float4.go
+++ pgtype/float4.go
@@ -3,6 +3,7 @@ package pgtype
 import (
 	"database/sql/driver"
 	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"math"
 	"strconv"
@@ -63,6 +64,29 @@ func (f Float4) Value() (driver.Value, error) {
 		return nil, nil
 	}
 	return float64(f.Float32), nil
+}
+
+func (f Float4) MarshalJSON() ([]byte, error) {
+	if !f.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(f.Float32)
+}
+
+func (f *Float4) UnmarshalJSON(b []byte) error {
+	var n *float32
+	err := json.Unmarshal(b, &n)
+	if err != nil {
+		return err
+	}
+
+	if n == nil {
+		*f = Float4{}
+	} else {
+		*f = Float4{Float32: *n, Valid: true}
+	}
+
+	return nil
 }
 
 type Float4Codec struct{}
