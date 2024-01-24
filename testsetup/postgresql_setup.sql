bf1c1d7848c087c99fb30a67657a5ec3197f7f32
Florent Viel <fviel@scaleway.com>
2024-01-24 09:21:24 +0100

    create ltree extension in pg setup for tests

--- testsetup/postgresql_setup.sql
+++ testsetup/postgresql_setup.sql
@@ -1,5 +1,6 @@
 -- Create extensions and types.
 create extension hstore;
+create extension ltree;
 create domain uint64 as numeric(20,0);
 
 -- Create users for different types of connections and authentication.
