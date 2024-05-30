jackc/pgx

commit 01d649b2bf9b87656f60f57bb495b45a21cf9023

Do not encode interval microseconds when they are 0
This make the encode match what postgres does

pgtype/interval.go
@@ -132,22 +132,31 @@ func (encodePlanIntervalCodecText) Encode(value any, buf []byte) (newBuf []byte,
 
 	if interval.Days != 0 {
 		buf = append(buf, strconv.FormatInt(int64(interval.Days), 10)...)
-		buf = append(buf, " day "...)
+		buf = append(buf, " day"...)
 	}
 
-	absMicroseconds := interval.Microseconds
-	if absMicroseconds < 0 {
-		absMicroseconds = -absMicroseconds
-		buf = append(buf, '-')
+	if interval.Microseconds != 0 {
+		buf = append(buf, " "...)
+
+		absMicroseconds := interval.Microseconds
+		if absMicroseconds < 0 {
+			absMicroseconds = -absMicroseconds
+			buf = append(buf, '-')
+		}
+
+		hours := absMicroseconds / microsecondsPerHour
+		minutes := (absMicroseconds % microsecondsPerHour) / microsecondsPerMinute
+		seconds := (absMicroseconds % microsecondsPerMinute) / microsecondsPerSecond
+
+		timeStr := fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+		buf = append(buf, timeStr...)
+
+		microseconds := absMicroseconds % microsecondsPerSecond
+		if microseconds != 0 {
+			buf = append(buf, fmt.Sprintf(".%06d", microseconds)...)
+		}
 	}
 
-	hours := absMicroseconds / microsecondsPerHour
-	minutes := (absMicroseconds % microsecondsPerHour) / microsecondsPerMinute
-	seconds := (absMicroseconds % microsecondsPerMinute) / microsecondsPerSecond
-	microseconds := absMicroseconds % microsecondsPerSecond
-
-	timeStr := fmt.Sprintf("%02d:%02d:%02d.%06d", hours, minutes, seconds, microseconds)
-	buf = append(buf, timeStr...)
 	return buf, nil
 }
 

commit 9f4a264f89a3157a35097556542dbd34124d7f8a

Fix interval encoding to allow 0s and avoid extra spaces
Fix a bugs introduced by 01d649b, also add some tests

pgtype/interval.go
@@ -132,29 +132,25 @@ func (encodePlanIntervalCodecText) Encode(value any, buf []byte) (newBuf []byte,
 
 	if interval.Days != 0 {
 		buf = append(buf, strconv.FormatInt(int64(interval.Days), 10)...)
-		buf = append(buf, " day"...)
+		buf = append(buf, " day "...)
 	}
 
-	if interval.Microseconds != 0 {
-		buf = append(buf, " "...)
+	absMicroseconds := interval.Microseconds
+	if absMicroseconds < 0 {
+		absMicroseconds = -absMicroseconds
+		buf = append(buf, '-')
+	}
 
-		absMicroseconds := interval.Microseconds
-		if absMicroseconds < 0 {
-			absMicroseconds = -absMicroseconds
-			buf = append(buf, '-')
-		}
+	hours := absMicroseconds / microsecondsPerHour
+	minutes := (absMicroseconds % microsecondsPerHour) / microsecondsPerMinute
+	seconds := (absMicroseconds % microsecondsPerMinute) / microsecondsPerSecond
 
-		hours := absMicroseconds / microsecondsPerHour
-		minutes := (absMicroseconds % microsecondsPerHour) / microsecondsPerMinute
-		seconds := (absMicroseconds % microsecondsPerMinute) / microsecondsPerSecond
+	timeStr := fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
+	buf = append(buf, timeStr...)
 
-		timeStr := fmt.Sprintf("%02d:%02d:%02d", hours, minutes, seconds)
-		buf = append(buf, timeStr...)
-
-		microseconds := absMicroseconds % microsecondsPerSecond
-		if microseconds != 0 {
-			buf = append(buf, fmt.Sprintf(".%06d", microseconds)...)
-		}
+	microseconds := absMicroseconds % microsecondsPerSecond
+	if microseconds != 0 {
+		buf = append(buf, fmt.Sprintf(".%06d", microseconds)...)
 	}
 
 	return buf, nil

pgtype/interval_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/jackc/pgx/v5/pgtype"
 	"github.com/jackc/pgx/v5/pgxtest"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestIntervalCodec(t *testing.T) {
@@ -135,4 +136,23 @@ func TestIntervalCodec(t *testing.T) {
 		{pgtype.Interval{}, new(pgtype.Interval), isExpectedEq(pgtype.Interval{})},
 		{nil, new(pgtype.Interval), isExpectedEq(pgtype.Interval{})},
 	})
+}
+
+func TestIntervalTextEncode(t *testing.T) {
+	m := pgtype.NewMap()
+
+	successfulTests := []struct {
+		source pgtype.Interval
+		result string
+	}{
+		{source: pgtype.Interval{Months: 2, Days: 1, Microseconds: 0, Valid: true}, result: "2 mon 1 day 00:00:00"},
+		{source: pgtype.Interval{Months: 0, Days: 0, Microseconds: 0, Valid: true}, result: "00:00:00"},
+		{source: pgtype.Interval{Months: 0, Days: 0, Microseconds: 6 * 60 * 1000000, Valid: true}, result: "00:06:00"},
+		{source: pgtype.Interval{Months: 0, Days: 1, Microseconds: 6*60*1000000 + 30, Valid: true}, result: "1 day 00:06:00.000030"},
+	}
+	for i, tt := range successfulTests {
+		buf, err := m.Encode(pgtype.DateOID, pgtype.TextFormatCode, tt.source, nil)
+		assert.NoErrorf(t, err, "%d", i)
+		assert.Equalf(t, tt.result, string(buf), "%d", i)
+	}
 }