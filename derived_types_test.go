b770252a3ba896a9a3d72f3de9f48029aa2ecb00
Nick Farrell <nick.farrell@aiven.io>
2024-07-07 11:19:30 +1000

    Fix bug relating to reuse of types
    When `LoadTypes` is being called, it does not include the
    namespace-qualified types in its result. While these namespaces are
    visible to `LoadTypes` itself, `RegisterTypes` will not recognise this
    form of the types, only allowing them to be used if they are on the
    schema path, and referred to without their namespace component.

--- derived_types_test.go
+++ derived_types_test.go
@@ -29,9 +29,12 @@ create type dtype_test as (
 
 		types, err := conn.LoadTypes(ctx, []string{"dtype_test"})
 		require.NoError(t, err)
-		require.Len(t, types, 3)
-		require.Equal(t, types[0].Name, "anotheruint64")
-		require.Equal(t, types[1].Name, "_anotheruint64")
-		require.Equal(t, types[2].Name, "dtype_test")
+		require.Len(t, types, 6)
+		require.Equal(t, types[0].Name, "public.anotheruint64")
+		require.Equal(t, types[1].Name, "anotheruint64")
+		require.Equal(t, types[2].Name, "public._anotheruint64")
+		require.Equal(t, types[3].Name, "_anotheruint64")
+		require.Equal(t, types[4].Name, "public.dtype_test")
+		require.Equal(t, types[5].Name, "dtype_test")
 	})
 }
