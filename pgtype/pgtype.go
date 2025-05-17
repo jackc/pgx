de7fe81d78655c58bc41427fef4c32c317b20884
Jack Christensen <jack@jackchristensen.com>
2025-05-17 09:11:31 -0500

    Use reflect.TypeFor instead of reflect.TypeOf
    Simplified function became available in Go 1.22.

--- pgtype/pgtype.go
+++ pgtype/pgtype.go
@@ -2032,14 +2032,7 @@ func (w *sqlScannerWrapper) Scan(src any) error {
 	return w.m.Scan(t.OID, TextFormatCode, bufSrc, w.v)
 }
 
-// valuerReflectType is a reflect.Type for driver.Valuer. It has confusing syntax because reflect.TypeOf returns nil
-// when it's argument is a nil interface value. So we use a pointer to the interface and call Elem to get the actual
-// type. Yuck.
-//
-// This can be simplified in Go 1.22 with reflect.TypeFor.
-//
-// var valuerReflectType = reflect.TypeFor[driver.Valuer]()
-var valuerReflectType = reflect.TypeOf((*driver.Valuer)(nil)).Elem()
+var valuerReflectType = reflect.TypeFor[driver.Valuer]()
 
 // isNilDriverValuer returns true if value is any type of nil unless it implements driver.Valuer. *T is not considered to implement
 // driver.Valuer if it is only implemented by T.
