562761a0830a339d8a160ef7a10a9e06ab2a6d15
Li Zeghong <zeghong@hotmail.com>
2025-08-15 15:46:46 +0800

    Ensure zeronull int types implement Int64Scanner
    Also correct error string when comparing minimum value.

--- pgtype/zeronull/int.go
+++ pgtype/zeronull/int.go
@@ -15,19 +15,19 @@ type Int2 int16
 func (Int2) SkipUnderlyingTypePlan() {}
 
 // ScanInt64 implements the Int64Scanner interface.
-func (dst *Int2) ScanInt64(n int64, valid bool) error {
-	if !valid {
+func (dst *Int2) ScanInt64(n pgtype.Int8) error {
+	if !n.Valid {
 		*dst = 0
 		return nil
 	}
 
-	if n < math.MinInt16 {
-		return fmt.Errorf("%d is greater than maximum value for Int2", n)
+	if n.Int64 < math.MinInt16 {
+		return fmt.Errorf("%d is less than minimum value for Int2", n.Int64)
 	}
-	if n > math.MaxInt16 {
-		return fmt.Errorf("%d is greater than maximum value for Int2", n)
+	if n.Int64 > math.MaxInt16 {
+		return fmt.Errorf("%d is greater than maximum value for Int2", n.Int64)
 	}
-	*dst = Int2(n)
+	*dst = Int2(n.Int64)
 
 	return nil
 }
@@ -63,19 +63,19 @@ type Int4 int32
 func (Int4) SkipUnderlyingTypePlan() {}
 
 // ScanInt64 implements the Int64Scanner interface.
-func (dst *Int4) ScanInt64(n int64, valid bool) error {
-	if !valid {
+func (dst *Int4) ScanInt64(n pgtype.Int8) error {
+	if !n.Valid {
 		*dst = 0
 		return nil
 	}
 
-	if n < math.MinInt32 {
-		return fmt.Errorf("%d is greater than maximum value for Int4", n)
+	if n.Int64 < math.MinInt32 {
+		return fmt.Errorf("%d is less than minimum value for Int4", n.Int64)
 	}
-	if n > math.MaxInt32 {
-		return fmt.Errorf("%d is greater than maximum value for Int4", n)
+	if n.Int64 > math.MaxInt32 {
+		return fmt.Errorf("%d is greater than maximum value for Int4", n.Int64)
 	}
-	*dst = Int4(n)
+	*dst = Int4(n.Int64)
 
 	return nil
 }
@@ -111,19 +111,19 @@ type Int8 int64
 func (Int8) SkipUnderlyingTypePlan() {}
 
 // ScanInt64 implements the Int64Scanner interface.
-func (dst *Int8) ScanInt64(n int64, valid bool) error {
-	if !valid {
+func (dst *Int8) ScanInt64(n pgtype.Int8) error {
+	if !n.Valid {
 		*dst = 0
 		return nil
 	}
 
-	if n < math.MinInt64 {
-		return fmt.Errorf("%d is greater than maximum value for Int8", n)
+	if n.Int64 < math.MinInt64 {
+		return fmt.Errorf("%d is less than minimum value for Int8", n.Int64)
 	}
-	if n > math.MaxInt64 {
-		return fmt.Errorf("%d is greater than maximum value for Int8", n)
+	if n.Int64 > math.MaxInt64 {
+		return fmt.Errorf("%d is greater than maximum value for Int8", n.Int64)
 	}
-	*dst = Int8(n)
+	*dst = Int8(n.Int64)
 
 	return nil
 }
