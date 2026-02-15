f8eb0d0596742c92beb1c22e9bad95a4ecb98b09
Jelte Fennema-Nio <github-tech@jeltef.nl>
2026-02-15 10:52:53 +0100

    Skip standard_conforming_strings=off on PG19
    For the same security reason why standard_conforming_strings=off is
    disallowed by pgx in combination with the simple query protocol, in PG19
    standard_conforming_strings=off won't be allowed anymore at all.
    
    See: https://github.com/postgres/postgres/commit/45762084545ec14dbbe66ace1d69d7e89f8978ac

--- pgxtest/pgxtest.go
+++ pgxtest/pgxtest.go
@@ -171,3 +171,21 @@ func SkipPostgreSQLVersionLessThan(t testing.TB, conn *pgx.Conn, minVersion int6
 		t.Skipf("Test requires PostgreSQL v%d+", minVersion)
 	}
 }
+
+func SkipPostgreSQLVersionGreaterThan(t testing.TB, conn *pgx.Conn, maxVersion int64) {
+	serverVersionStr := conn.PgConn().ParameterStatus("server_version")
+	serverVersionStr = regexp.MustCompile(`^[0-9]+`).FindString(serverVersionStr)
+	// if not PostgreSQL do nothing
+	if serverVersionStr == "" {
+		return
+	}
+
+	serverVersion, err := strconv.ParseInt(serverVersionStr, 10, 64)
+	if err != nil {
+		t.Fatalf("postgres version parsed failed: %s", err)
+	}
+
+	if serverVersion > maxVersion {
+		t.Skipf("Test requires PostgreSQL v%d or lower", maxVersion)
+	}
+}
