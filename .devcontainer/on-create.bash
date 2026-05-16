b7421c678360659f48828e6ee7405c4386fc71ac
Jack Christensen <jack@jackchristensen.com>
2026-05-16 14:58:27 +0000

    Use mise in devcontainer

--- .devcontainer/on-create.bash
+++ .devcontainer/on-create.bash
@@ -3,3 +3,6 @@ set -e
 
 sudo chown vscode:vscode /persist/local /persist/shared
 mkdir -p /persist/shared/{claude,atuin/{config,data},go,go-cache,mise/{data,cache},psql,devcontainer-downloads}
+
+mise trust
+mise install
